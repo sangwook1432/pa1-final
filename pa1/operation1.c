ba5eb43486e7d4d9cf97e2dbc6bd2a2643e6463d
sangwook1432 <103094090+sangwook1432@users.noreply.github.com>
2025-10-04 19:33:03 +0900

    Update operation1.c

--- pa1/operation1.c
+++ pa1/operation1.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #define MAX_LEN 8192
 
@@ -31,13 +32,34 @@ void search_in_line1(char* line_buffer, const char* query, uint64_t line_number)
 
 void operation1(const char* filename,const char* query){
     int fd = open(filename,O_RDONLY);
-    if(fd < 0) return;
+    if(fd < 0) {
+        write(STDOUT_FILENO, "\n", 1);
+        return;
+    }
+
+    size_t capacity = MAX_LEN;
+    char* line_buffer = malloc(sizeof(char) * capacity);
+    if (line_buffer == NULL) {
+        close(fd);
+        write(STDOUT_FILENO, "\n", 1);
+        return;
+    }
     char buffer[1];
-    char line_buffer[MAX_LEN];
     int line_idx = 0;
     uint64_t line_number = 1;
 
     while(read(fd,buffer,1) > 0){
+        if (line_idx >= capacity - 1) {
+            capacity *= 2;
+            char* new_buffer = realloc(line_buffer, capacity);
+            if (new_buffer == NULL) {
+                free(line_buffer);
+                close(fd);
+                write(STDOUT_FILENO, "\n", 1);
+                return;
+            }
+            line_buffer = new_buffer;
+        }
         if(buffer[0] != '\n'){
             if(line_idx < MAX_LEN - 1){
                 line_buffer[line_idx++] = buffer[0];
@@ -55,6 +77,7 @@ void operation1(const char* filename,const char* query){
         line_buffer[line_idx] = '\0';
         search_in_line1(line_buffer,query,line_number);
     }
+    free(line_buffer);
     write(STDOUT_FILENO, "\n", 1);
     close(fd);
 }
